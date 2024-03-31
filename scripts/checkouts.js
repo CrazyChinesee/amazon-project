d2d9e42b9faadee2350b812bb1a7b36b4c064a46
Nikola <nikolaharmonikas@gmail.com>
2024-03-31 23:27:00 +0200

    Creating Utils money.js

--- scripts/checkouts.js
+++ scripts/checkouts.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = "";
 
@@ -28,9 +29,13 @@ cart.forEach((cartItem) => {
           <div class="product-name">
             ${matchingProduct.name}
           </div>
-          <div class="product-price">$${matchingProduct.priceCents}</div>
+          <div class="product-price">$${formatCurrency(
+            matchingProduct.priceCents
+          )}</div>
           <div class="product-quantity">
-            <span> Quantity: <span class="quantity-label">${cartItem.quantity}</span> </span>
+            <span> Quantity: <span class="quantity-label">${
+              cartItem.quantity
+            }</span> </span>
             <span class="update-quantity-link link-primary">
               Update
             </span>
