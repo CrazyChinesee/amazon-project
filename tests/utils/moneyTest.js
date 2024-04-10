9c96df4a9455e38aad38d75bd6bb233e04d4a040
Nikola <nikolaharmonikas@gmail.com>
2024-04-10 18:39:39 +0200

    16b

--- tests/utils/moneyTest.js
+++ tests/utils/moneyTest.js
@@ -16,4 +16,8 @@ describe("test suite: formatCurrency", () => {
   it("rounds down to the nearest cent(<.5)", () => {
     expect(formatCurrency(2000.4)).toEqual("20.00");
   });
+
+  it("works with negative numbers", () => {
+    expect(formatCurrency(-5200)).toEqual("-52.00");
+  });
 });
