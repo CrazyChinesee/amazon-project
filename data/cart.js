d2fdd13207242595da43ca755aee95c4ee5fa2bf
Nikola <nikolaharmonikas@gmail.com>
2024-04-10 13:44:09 +0200

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,19 +1,25 @@
-export let cart = JSON.parse(localStorage.getItem("amazon_cart"));
+export let cart;
 
-if (!cart) {
-  //When running for first time we expect cart to return null because its empty variable.
-  cart = [
-    {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2,
-      deliveryOptionId: "1",
-    },
-    {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1,
-      deliveryOptionId: "2",
-    },
-  ];
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem("amazon_cart"));
+
+  if (!cart) {
+    //When running for first time we expect cart to return null because its empty variable.
+    cart = [
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: "1",
+      },
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: "2",
+      },
+    ];
+  }
 }
 
 function saveToStorage() {
